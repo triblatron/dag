16eb169d61cf898ae04fd94f599aab15767b5143
Tony Horrobin <tony.horrobin@gmail.com>
2024-06-02 21:07:32 +0100

    doc:Add doxygen comments for front-end operations feat:Add undo, redo operations.

--- include/NodeEditorFrontEndTUI.h
+++ include/NodeEditorFrontEndTUI.h
@@ -33,17 +33,30 @@ namespace nbe
             OP_LOAD_BIT = 1<<0,
             //! Save a graph to the persistent format.
             OP_SAVE_BIT = 1<<1,
+            //! Create a node with a given name, class
             OP_CREATE_NODE = 1<<2,
+            //! Delete a Node by name
             OP_DELETE_NODE = 1<<3,
+            //! Add a dynamic Port to a Node
             OP_CREATE_PORT = 1<<4,
+            //! Delete a dynamic Port from a Node
             OP_DELETE_PORT = 1<<5,
+            //! Create a SignalPath linking two Ports
             OP_CREATE_SIGNALPATH = 1<<6,
+            //! Delete a SignalPath
             OP_DELETE_SIGNALPATH = 1<<7,
+            //! Create a child Graph from the selection
             OP_CREATE_CHILD = 1<<8,
+            //! Delete a child Graph
             OP_DELETE_CHILD = 1<<9,
+            //! Select a list of Nodes using the given mode
             OP_SELECT = 1<<10,
             //! Run the graph.
             OP_RUN_BIT  = 1<<11,
+            //! Undo the last operation
+            OP_UNDO_BIT = 1<<12,
+            //! Redo the last undone operation
+            OP_REDO_BIT = 1<<13,
             //! All operations.
             OP_ALL_BITS = ~0U
         };
