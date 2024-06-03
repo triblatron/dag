86b54a70e2c910b8200141e2d49fc8b701a5480b
Tony Horrobin <tony.horrobin@gmail.com>
2024-06-03 22:45:02 +0100

    feat:First cut at BackEnd interface as opposed to NodeEditorInterface.

---
+++ include/NodeEditorBackEnd.h
@@ -0,0 +1,27 @@
+//
+// Created by tony on 03/06/24.
+//
+
+#pragma once
+
+#include "config/Export.h"
+#include "Types.h"
+
+#include <string>
+
+namespace nbe
+{
+    class NodeEditorFrontEnd;
+
+    class NBE_API NodeEditorBackEnd
+    {
+    public:
+        virtual void createNode(const std::string& className, const std::string& name) = 0;
+
+        virtual void deleteNode(const std::string& name) = 0;
+
+        virtual void addPort(const std::string& nodeName, const std::string& portClass, PortType type, PortDirection direction) = 0;
+
+        virtual void deletePort(const std::string& nodeName, size_t portIndex) = 0;
+    };
+}
