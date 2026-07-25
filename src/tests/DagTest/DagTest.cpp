eaacce6fb68f34d520be3a253bebcac1bda5f38d
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2026-07-25 13:24:56 +0100

    test,data:DagTest/DagTest.cpp, connectednodesnonzeroid.lua, LoadConnectedNonZeroIdThenCreateNode.lua:Add test for a connected graph with non-zero identifiers. submodule:doc:Remove misleading comment.

--- src/tests/DagTest/DagTest.cpp
+++ src/tests/DagTest/DagTest.cpp
@@ -1580,6 +1580,7 @@ TEST_P(NodeEditorLive_testScripted, testExpectedValue)
 INSTANTIATE_TEST_SUITE_P(NodeEditorLive, NodeEditorLive_testScripted, ::testing::Values(
     std::make_tuple("etc/tests/NodeEditorLive/SaveOneNode.lua"),
     std::make_tuple("etc/tests/NodeEditorLive/LoadConnected.lua"),
+    std::make_tuple("etc/tests/NodeEditorLive/LoadConnectedNonZeroIdThenCreateNode.lua"),
     std::make_tuple("etc/tests/NodeEditorLive/LoadNonZeroIdStatic.lua"),
     std::make_tuple("etc/tests/NodeEditorLive/LoadNonZeroIdDynamic.lua"),
     std::make_tuple("etc/tests/NodeEditorLive/LoadNonZeroIdHybrid.lua"),
