42f34ad51e6b9ced93aed3a9ffb5c4d8c13a1aef
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2026-08-03 19:35:09 +0100

    test,refactor:Move CopyOp tests into DagBase. submodule:Fixes for cloning whole Graphs versus Nodes within the same Graph.

--- src/tests/DagTest/RoundTripTest.cpp
+++ src/tests/DagTest/RoundTripTest.cpp
@@ -2,34 +2,10 @@
 // Created by Tony Horrobin on 04/04/2026.
 //
 
-#include "../../../thirdparty/dagbase/include/core/Types.h"
+#include "core/Types.h"
 #include <gtest/gtest.h>
 
 #include "NodeEditorInterface.h"
-
-class CopyOp_testRoundTrip : public ::testing::TestWithParam<std::tuple<std::string, dagbase::CopyOp>>
-{
-
-};
-
-TEST_P(CopyOp_testRoundTrip, testRoundTrip)
-{
-    auto str = std::get<0>(GetParam());
-    auto value = std::get<1>(GetParam());
-
-    EXPECT_EQ(str, dagbase::copyOpToString(value));
-    EXPECT_EQ(value, dagbase::parseCopyOp(str));
-}
-
-INSTANTIATE_TEST_SUITE_P(CopyOp, CopyOp_testRoundTrip, ::testing::Values(
-    std::make_tuple("DEEP_COPY_NONE", dagbase::DEEP_COPY_NONE),
-    std::make_tuple("DEEP_COPY_NODES_BIT", dagbase::DEEP_COPY_NODES_BIT),
-    std::make_tuple("DEEP_COPY_INPUTS_BIT", dagbase::DEEP_COPY_INPUTS_BIT),
-    std::make_tuple("DEEP_COPY_OUTPUTS_BIT", dagbase::DEEP_COPY_OUTPUTS_BIT),
-    std::make_tuple("GENERATE_UNIQUE_ID_BIT", dagbase::GENERATE_UNIQUE_ID_BIT),
-    std::make_tuple("DEEP_COPY_PARENT_BIT", dagbase::DEEP_COPY_PARENT_BIT),
-    std::make_tuple("DEEP_COPY_NODES_BIT DEEP_COPY_INPUTS_BIT DEEP_COPY_OUTPUTS_BIT", static_cast<dagbase::CopyOp>(dagbase::DEEP_COPY_NODES_BIT|dagbase::DEEP_COPY_INPUTS_BIT|dagbase::DEEP_COPY_OUTPUTS_BIT))
-    ));
     // DEEP_COPY_INPUTS_BIT    = 1<<1,
     // DEEP_COPY_OUTPUTS_BIT   = 1<<2,
     // GENERATE_UNIQUE_ID_BIT  = 1<<3,
