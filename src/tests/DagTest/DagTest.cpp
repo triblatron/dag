b8fefa0285f68dd707d92b17ac86745cad78e472
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2026-09-07 21:14:30 +0100

    test,data:TopoSortTransitiveDependency.lua, TopoSortCyclicDependency.lua:Add tests for transitive and cyclic dependencies while attempt a topological sort.

--- src/tests/DagTest/DagTest.cpp
+++ src/tests/DagTest/DagTest.cpp
@@ -1431,6 +1431,8 @@ TEST_P(NodeEditorLive_testScripted, testExpectedValue)
 }
 
 INSTANTIATE_TEST_SUITE_P(NodeEditorLive, NodeEditorLive_testScripted, ::testing::Values(
+    std::make_tuple("etc/tests/NodeEditorLive/TopoSortCyclicDependency.lua"),
+    std::make_tuple("etc/tests/NodeEditorLive/TopoSortTransitiveDependency.lua"),
     std::make_tuple("etc/tests/NodeEditorLive/CreateChildFanOut.lua"),
     std::make_tuple("etc/tests/NodeEditorLive/TopoSortAfterDeserialisation.lua"),
     std::make_tuple("etc/tests/NodeEditorLive/TopoSort.lua"),
